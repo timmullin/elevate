df200feec91e0e8bd5e1f0e508d960571df9648b
Tim Mullin <tim@cpanel.net>
2024-03-15 20:43:10 +0000

    Moved function that parses stdout from leapp

--- lib/Elevate/Blockers/Leapp.pm
+++ lib/Elevate/Blockers/Leapp.pm
@@ -68,67 +68,13 @@ sub check ($self) {
     # any non-fatal conditions reported there.  So, we need to fish
     # them from stdout.
 
-    my $error_block = _extract_error_block_from_output( $out->{stdout} );
+    my $error_block = $self->cpev->leapp->extract_error_block_from_output( $out->{stdout} );
 
     if ( length $error_block ) {
         $self->has_blocker( "Leapp encountered the following error(s):\n" . $error_block );
     }
 
     return;
-}
-
-sub _extract_error_block_from_output ($text_ar) {
-
-    # The fatal errors will appear there in a block that looks like this:
-    # =========================================
-    #               ERRORS
-    # =========================================
-    #
-    # Info about the errors
-    #
-    # =========================================
-    #             END OF ERRORS
-    # =========================================
-
-    my $error_block = '';
-
-    my $found_banner_line        = 0;
-    my $found_second_banner_line = 0;
-    my $in_error_block           = 0;
-
-    foreach my $line (@$text_ar) {
-
-        # Keep looking for a "banner" line (a line full of "=")
-        if ( !$found_banner_line ) {
-            $found_banner_line = 1 if $line =~ /^={10}/;
-            next;
-        }
-
-        # We've found the banner line, check if this is the error block
-        if ( !$in_error_block ) {
-            if ( $line =~ /^\s+ERRORS/ ) {
-                $in_error_block = 1;
-            }
-            else {
-                # not the error block, go back to looking for a banner line
-                $found_banner_line = 0;
-            }
-            next;
-        }
-
-        # We can't start harvesting the error info until we pass the second banner line
-        if ( !$found_second_banner_line ) {
-            $found_second_banner_line = 1 if $line =~ /^={10}/;
-            next;
-        }
-
-        # If we come across another banner line, we are done with the error block
-        last if $line =~ /^={10}/;
-
-        $error_block .= $line;
-    }
-
-    return $error_block;
 }
 
 1;
